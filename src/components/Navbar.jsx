9b9467f12a78ebd5a4427123bbf06e1ba56161e2
unknown <nderim258@gmail.com>
2024-02-10 13:04:00 +0100

    Category cards added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
-          Better <span className="font-bold ">Eat</span>
+          Better<span className="font-bold ">Eat</span>
         </h1>
         <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
           <p className="bg-black text-white rounded-full p-2">Delivery</p>
@@ -42,7 +42,10 @@ const Navbar = () => {
       {/* Mobile Menu */}
 
       {nav && (
-        <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0" />
+        <div
+          onClick={() => setNav(false)}
+          className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"
+        />
       )}
 
       <div
@@ -58,7 +61,7 @@ const Navbar = () => {
           className="absolute right-4 top-4 cursor-pointer"
         />
         <h2 className="text-2xl p-4">
-          Best <span className="font-bold">Eats</span>
+          Better<span className="font-bold">Eat</span>
         </h2>
         <nav>
           <ul className="flex flex-col p-4 text-gray-800">
